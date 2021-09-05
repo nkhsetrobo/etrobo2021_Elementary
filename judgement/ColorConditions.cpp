dcb209de311b5ec93adf5bd8ed28ad34252212fb
Ryuchama <nkhs.hc39.1077@gmail.com>
2021-09-05 21:32:45 +0900

    色判定
    青を判定？するようなプログラムの作成

--- judgement/ColorConditions.cpp
+++ judgement/ColorConditions.cpp
@@ -4,7 +4,12 @@ ColorConditions::ColorConditions()
     :judgement()
 
 bool ColorConditions::judge(){
-
+    //青を判定？
+    if(aBrightness > /*mColorSensor->getValue()*/){
+        return  true;
+    } else {
+        return false;
+    }
 
 }
 
@@ -16,7 +21,7 @@ bool ColorConditions::judge(){
 
 void ColorConditions::setParameter(float Brightness[]){
     //計測器から自己位置を取得
-    //mBrightness = ->getValue();
+    //mBrightness = mColorSensor->getValue();
     //パラメータを取得  ※相対座標：Miles[0]、絶対座標：Miles[1] 予定
     aBrightness = Brightness[0];
 
