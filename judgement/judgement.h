56508be5f8c188c37d729abd26af71495011bfd7
Ryuchama <nkhs.hc39.1077@gmail.com>
2021-09-05 16:13:23 +0900

    判定(親クラス)
    変数の誤りの修正、未実装箇所の追加

--- judgement/judgement.h
+++ judgement/judgement.h
@@ -1,10 +1,12 @@
-#ifndef JUDGE_H_
-#define JUDGE_H_
+#ifndef JUDGEMENT_H_
+#define JUDGEMENT_H_
 #include "MySensor.h"
 #include "TurnAngSensor.h"
-#include "ArmAngSensor.h"
-#include "TailAngSensor.h"
+//#include "ArmAngSensor.h"
+//#include "TailAngSensor.h"
 #include "MileageSensor.h"
+
+
 
 class judgement {
     public:
@@ -15,10 +17,10 @@ class judgement {
 
     protected:
    
-        MileageCondition    *mMileageCondition;
-        TurnAngCondition    *mTurnAngCondition;
-        ArmAngCondition     *mArmAngCondition;
-        TailAngCondition    *mTailAngCondition;
+        MileageSensor            *mMileageSensor;
+        TurnAngSensor            *mTurnAngSensor;
+        //ArmAngConditions     *mArmAngConditions;
+        //TailAngConditions    *mTailAngConditions;
         
 };
 
