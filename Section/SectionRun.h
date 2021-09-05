95695e7bed064d9d093a4867ed6fb52e0b73ab7a
Ryuchama <nkhs.hc39.1077@gmail.com>
2021-09-05 15:56:02 +0900

    区間走行
    現最新版。
    区間用パラメータ作成

--- Section/SectionRun.h
+++ Section/SectionRun.h
@@ -2,15 +2,42 @@
 #define SECTION_RUN
 
 #include "Walker.h"
+#include "SimpleWalker.h"
 #include "ScenarioCurveLineTracer.h"
 #include "ScenarioLineTracer.h"
 #include "LineTracer.h"
 
-class SectionRun : public SectionRun{
+#include "MileageConditions.h"
+#include "TurnAngConditions.h"
+#include "ArmAngConditions.h"
+#include "TailAngConditions.h"
+
+class SectionRun{
     public:
         SectionRun();
         ~SectionRun();
         bool Run();
-}
+        Walker *witchWalker(int part);
+
+        enum WALKER_PART{
+            SIMPWALKER,     //0
+            SCENARIOCURVE,  //1
+            SCENARIOLINE,   //2
+            LINETRACER      //3
+        };
+
+        enum JUGE_PART{
+            MILEAGE,    //0  ※距離条件
+            TURNANG,    //1　※旋回角度条件
+            ARMANG,     //2　※アーム角度条件
+            TAILANG     //3　※尻尾角度条件
+        };
+
+        private:
+            Walker      *mWalker;
+            Walker      *mWalkMethod[10];
+            judgement   *mJudgement;
+            judgement   *mJudgeMethod[10];
+};
 
 #endif
